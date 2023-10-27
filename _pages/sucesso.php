37c179f5413370e3c9e7cdda4a7a668e4efc63b2
jamilymagalhaess <magalhaes.jamily@hotmail.com>
2023-10-26 21:05:10 -0300

    jamily

--- _pages/sucesso.php
+++ _pages/sucesso.php
@@ -2,15 +2,17 @@
 <html>
 <head>
     <title>Tela de sucesso</title>
-    <link rel="stylesheet" type="text/css" href="../_estilos/estilosLog.css">
+    <link rel="stylesheet" type="text/css" href="../_estilos/estiloSuc.css">
 </head>
 
 <body>
     <div class="sucesso">
-        <h2>sucesso</h2>
-
-        <a href="../_pages/cad.php"><button> retornar macaco</button></a>
-        
+        <fieldset >
+            <div >    
+                <h2>cadastro realizado com sucesso!!!</h2>
+            <a href="../_pages/cad.php"><button class="botao"> Voltar</button></a>
+            </div>
+        </fieldset>   
     </div>
 </body>
 
