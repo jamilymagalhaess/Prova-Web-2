320bac2a5796ce8976cefc2e77b626e61e333507
jamilymagalhaess <magalhaes.jamily@hotmail.com>
2023-10-26 21:40:57 -0300

    jamily

---
+++ _pages/cabecalho.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MedMais - Medicamentos</title>
+    <style>
+        body {
+            font-family: sans-serif;
+           
+        }
+        header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background-color: #007bff;
+            color: #fff;
+            padding: 20px;
+             background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+        }
+        h1 {
+            margin: 0;
+        }
+        .botao {
+            color: #fff;
+            text-decoration: none;
+            padding: 10px 20px;
+            background-image: linear-gradient(to right, rgb(0, 92, 197), rgb(90, 20, 220));
+            border-radius: 5px;
+            border: 0;
+            margin: 0 15px;
+        }
+       
+    </style>
+</head>
+<body>
+    <header>
+        <h1>D & J - Perfumes Cheirosos</h1>
+        <div>
+
+        <a href="../_pages/principal.php"><button class="botao"> Voltar</button></a>
+        <a href="../_pages/login.php"><button class="botao"> Fazer Login</button></a>
+    </div>
+    </header>
+</body>
+</html>
